15d22056a573ee2b02896b4f85f3d417a2e4e82c
MikaelSantilio <mikael.santilio@gmail.com>
2021-01-30 14:40:53 -0300

    adicao de bootstrap 5

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from core import views
 from django.contrib.auth import views as auth_views
 
+from django.contrib.auth import views as auth_views
+
 app_name = "core"
 
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('login/', views.LoginView.as_view(), name='login'),
+    # path('logout/', views.logout, name='logout'),
     
     # path('logout/', auth_views.logout, {'next_page': '/login'}),
     path('ver-nota/', views.VisualizarNotasControl.as_view(), name='ver-nota'),
--- core/views.py
+++ core/views.py
@@ -1,58 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import View, DetailView, FormView, RedirectView
-from core.models import Nota, Inscricao, Aluno
-from django.shortcuts import get_object_or_404
-from django.utils.http import is_safe_url
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login as auth_login, logout as auth_logout
-from django.views.decorators.cache import never_cache
-from django.views.decorators.csrf import csrf_protect
-from django.views.decorators.debug import sensitive_post_parameters
-from django.views.generic import FormView, RedirectView
-from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
-from django.conf import settings
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
+from django.views.generic import DetailView, View
 
-class LoginView(FormView):
-    """
-    Provides the ability to login as a user with a username and password
-    """
-    success_url = '/'
-    template_name = 'Login.html'
-    form_class = AuthenticationForm
-
-    @method_decorator(sensitive_post_parameters('password'))
-    @method_decorator(csrf_protect)
-    @method_decorator(never_cache)
-    def dispatch(self, request, *args, **kwargs):
-        # Sets a test cookie to make sure the user has cookies enabled
-        request.session.set_test_cookie()
-
-        return super(LoginView, self).dispatch(request, *args, **kwargs)
-
-    def form_valid(self, form):
-        auth_login(self.request, form.get_user())
-
-        # If the test cookie worked, go ahead and
-        # delete it since its no longer needed
-        if self.request.session.test_cookie_worked():
-            self.request.session.delete_test_cookie()
-
-        return super(LoginView, self).form_valid(form)
-
-    def get_success_url(self):
-        return reverse_lazy('core:ver-nota')
-
-
-def logout(request):
-    auth_logout(request)
-    return HttpResponseRedirect('/ver-notas')
-
-class LogoutView(View):
-    def get(self, request):
-        return HttpResponseRedirect(settings.LOGIN_URL)
+from core.models import Aluno, Inscricao, Nota
 
 
 @method_decorator(login_required, name='dispatch')
